17ec38e3301f45992c0f6e2dd536f040b03632ea
Anju-Alexander <anjusara102@gmail.com>
2023-04-19 18:14:45 +0530

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         for(int i=0;i<10;i++)
-        System.out.println("Hello world!");
+        System.out.println("Hello world Anju!");
     }
 }
